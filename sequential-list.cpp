e160884f2d04bb7f240663d3766207bc6b455c2c
rishivimal <rvimalen@uwaterloo.ca>
2020-05-31 15:06:37 -0400

    first commit

--- sequential-list.cpp
+++ sequential-list.cpp
@@ -1,66 +1,178 @@
 #include "sequential-list.h"
+#include <iostream>
 
 SequentialList::SequentialList(unsigned int cap)
 {
+    data_ = new int [cap];
+    capacity_ = cap;
 }
 
 SequentialList::~SequentialList()
 {
+    delete [] data_;
+    data_ = nullptr;
 }
 
 unsigned int SequentialList::size() const
 {
+    return size_;
 }
 
 unsigned int SequentialList::capacity() const
 {
+    return capacity_;
 }
 
 bool SequentialList::empty() const
 {
+    if (size_ == 0)
+        return true;
+
+    else
+        return false;
 }
 
 bool SequentialList::full() const
 {
+    if (size_ == capacity_)
+        return true;
+
+    else
+        return false;
 }
 
 SequentialList::DataType SequentialList::select(unsigned int index) const
 {
+    if (index < size_ && index >= 0){
+            return data_[index];
+    }
+
+    else
+        return data_[size_ - 1];
 }
 
 unsigned int SequentialList::search(DataType val) const
 {
+    for (int i = 0; i < size_; i++)
+    {
+        if (val == data_[i])
+            return i;
+    }
+    return size_;
 }
 
 void SequentialList::print() const
 {
+    for (int i = 0; i < size_; i++){
+        std::cout << data_ [i];
+    }
 }
 
 bool SequentialList::insert(DataType val, unsigned int index)
 {
+    if (full() || index < 0 || index > size_){
+        return false;
+    }
+
+    if (index == size_){
+        data_[index] = val;
+        size_ += 1;
+        return true;
+    }
+    else{
+        for (int i = size_; i > index; i--){
+            data_ [i] = data_[i - 1];
+        }
+        data_[index] = val;
+        size_+= 1;
+        return true;
+    }
 }
 
 bool SequentialList::insert_front(DataType val)
 {
+    if (full())
+        return false;
+
+    if (empty()){
+        data_[0] = val;
+        size_+= 1;
+        return true;
+    }
+
+    else{
+        for (int i = size_; i > 0; i--){
+            data_ [i] = data_[i - 1];
+        }
+        data_[0] = val;
+        size_+= 1;
+        return true;
+    }
 }
 
 bool SequentialList::insert_back(DataType val)
 {
+    if (full())
+        return false;
+
+    else{
+        data_[size_] = val;
+        size_+= 1;
+    }
 }
 
 bool SequentialList::remove(unsigned int index)
 {
+    if (index < 0 || index >= size_){
+        return false;
+    }
+
+    else{
+        for (int i = index; i < (size_ -1); i++){
+            data_[i] = data_ [i+1];
+        }
+        data_[size_ - 1] = NULL;
+        size_ -=1;
+        return true;
+    }
 }
 
 bool SequentialList::remove_front()
 {
+
+    if (empty())
+        return false;
+
+    else{
+        for (int i = 0; i < (size_ -1); i++){
+            data_[i] = data_ [i+1];
+        }
+        data_[size_ - 1] = NULL;
+        size_ -=1;
+        return true;
+    }
 }
 
 bool SequentialList::remove_back()
 {
+    if (empty())
+        return false;
+
+    else{
+        data_[size_-1] = NULL;
+        size_ -=1;
+        return true;
+    }
 }
 
 bool SequentialList::replace(unsigned int index, DataType val)
 {
+    if (index < 0 || index > size_){
+        return false;
+    }
+    else{
+        data_[index] = val;
+        return true;
+    }
 }
 
--- test.cpp
+++ test.cpp
@@ -4,7 +4,33 @@ properly by designing your own test case and test code.
 ****************************************************/
 
 #include <iostream>
+#include "sequential-list.h"
+using namespace std;
 
 int main() {
-    return 0;
+
+    SequentialList *list = new SequentialList(10);
+
+    list -> insert(5, 0);
+    list -> insert(7, 0);
+    list -> insert(1, 0);
+    list -> insert(3, 0);
+    list -> insert(2, 0);
+    list -> insert(8, 2);
+    list -> insert_front(4);
+    list -> insert_back(9);
+    list -> insert_front(6);
+    list -> insert_front(0);
+    list -> insert_front(20);
+    //list -> replace(9, 5);
+
+    cout << list -> search(2);
+    cout << "\n";
+    cout << list -> select(0);
+    cout << "\n";
+    cout << list -> size();
+    cout << "\n";
+    list -> print();
+    delete list;
+    list = nullptr;
 }
